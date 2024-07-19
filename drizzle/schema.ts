badaa7c63019b15d45cc2f5425758e132facd452
Say Glenn <glennsay1234@gmail.com>
2024-07-19 13:11:51 +0800

    drizzle setup

---
+++ drizzle/schema.ts
@@ -0,0 +1,26 @@
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from '@vercel/postgres';
+import {
+  pgTable,
+  serial,
+  text,
+  numeric,
+  customType,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core';
+
+const bytea = customType<{ data: Buffer; notNull: false; default: false }>({
+  dataType() {
+    return "bytea";
+  },
+});
+
+export const FameTable = pgTable(
+  'players',
+  {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    image: bytea('image').notNull(),
+    week: numeric('week').notNull(),
+  }
+)
