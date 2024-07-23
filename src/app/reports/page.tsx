e1da4d2bb733b76b11ee658637990efd3054737c
Say Glenn <glennsay1234@gmail.com>
2024-07-23 23:04:25 +0800

    fix imports

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,6 +1,6 @@
-import ReportsFooter from "@/components/reports/ReportsFooter";
-import ReportsHeader from "@/components/reports/ReportsHeader";
-import ReportsLinks from "@/components/reports/ReportsLinks";
+import ReportsFooter from "@/src/components/reports/ReportsFooter";
+import ReportsHeader from "@/src/components/reports/ReportsHeader";
+import ReportsLinks from "@/src/components/reports/ReportsLinks";
 
 export default function Page() {
   return (
